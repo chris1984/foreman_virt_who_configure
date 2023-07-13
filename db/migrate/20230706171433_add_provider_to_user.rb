901db618ea3dbed795d08b4355485a1193d0026a
Chris Roberts <chrobert@redhat.com>
2023-07-13 15:44:11 -0400

    lah

--- db/migrate/20230706171433_add_provider_to_user.rb
+++ db/migrate/20230706171433_add_provider_to_user.rb
@@ -1,15 +1,23 @@
 class AddProviderToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :foreman_virt_who_configure_service_users, :provider, :string
+    add_column :foreman_virt_who_configure_service_users, :provider_type, :string
+    add_column :foreman_virt_who_configure_service_users, :provider_name, :string
+    add_column :foreman_virt_who_configure_service_users, :organization_id, :integer
 
     configs = ForemanVirtWhoConfigure::Config.all
+
     if configs.present?
       configs.each do |config|
         provider_type = config.hypervisor_type
+        provider_name = config.hypervisor_server
+        org_id = config.organization_id
         account = config.service_user_id
         service_user = ForemanVirtWhoConfigure::ServiceUser.where(id: account)
-        service_user.provider = provider_type
-        service_user.save
+
+        service_user.first.provider_type = provider_type
+        service_user.first.provider_name = provider_name
+        service_user.first.organization_id = org_id
+        service_user.first.save
       end
     end
   end
